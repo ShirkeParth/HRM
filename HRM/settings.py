50c22023ff2fe641281c038e5ba5c4753b8eb612
parth <parthshirke2004@gmail.com>
2026-06-15 00:25:49 +0530

    Changed database to SQLite for deployment

--- HRM/settings.py
+++ HRM/settings.py
@@ -75,12 +75,8 @@ WSGI_APPLICATION = 'HRM.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'hrm',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
